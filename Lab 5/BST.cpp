6fa63c651ba05f847bd85e9a3ef243eba37ba763
Kartik Gokhale <80845083+AWorldOfChaos@users.noreply.github.com>
2022-11-03 11:10:43 +0530

    Updated BST.cpp

--- Lab 5/BST.cpp
+++ Lab 5/BST.cpp
@@ -8,74 +8,206 @@
 #endif
 
 
-// Returns true on successful insertion in BST. Returns false otherwise.
-// You may assume that no two elements being inserted have the same journey
-// code, and journey code is the key for insertion and finding in the BST
-
 bool BST::insert(int jrnyCode, int price)
 {
   // Implement insertion in BST
+  if (!root)
+  {
+    root = new TreeNode(jrnyCode, price);
+    return true;
+  }
 
-  // You MUST use only BSTLessThan to compare two journey codes.  You
-  // can create a new node containing the journey code to be inserted
-  // and invoke BSTLessThan on the current node in the tree that you
-  // are inspecting and the new node to do your comparison.
+  TreeNode *current = root;
 
-  // REMEMBER to update the heights of appropriate nodes in the tree
-  // after an insertion.
-  
-  // We use "return true" below to enable compilation.  Change this as you
-  // see fit.
-  return true;
+  TreeNode *newNode = new TreeNode(jrnyCode, price);
+
+  while (true)
+  {
+    if (BSTLessThan(newNode, current))
+    {
+      if (!current->left)
+      {
+        updateBSTParentChild(newNode, current, true);
+        return true;
+      }
+      else
+      {
+        current = current->left;
+      }
+    }
+    else
+    {
+      if (!current->right)
+      {
+        updateBSTParentChild(newNode, current, false);
+        return true;
+      }
+      else
+      {
+        current = current->right;
+      }
+    }
+  }
+  return false;
 }
-
-// Returns the price on finding an element in the BST that matches
-// journey code. Returns -1 otherwise.
-// You may assume that no two node in the tree have the same journey
-// code, and journey code is the key for insertion and finding in the BST
 
 int BST::find(int jrnyCode)
 {
-  // Implement find in BST
+  TreeNode *currentNode = root;
+  TreeNode *searchNode = new TreeNode(jrnyCode, -1);
 
-  // You MUST use only BSTLessThan to compare two journey codes.  You
-  // can create a new node containing the journey code to be searched
-  // (along with some arbitrary price) and invoke BSTLessThan on the
-  // current node in the tree that you are inspecting and the new node
-  // to do your comparison.
+  while (currentNode != nullptr)
+  {
+    if (BSTLessThan(searchNode, currentNode))
+    {
+      currentNode = currentNode->left;
+    }
+    else if (BSTLessThan(currentNode, searchNode))
+    {
+      currentNode = currentNode->right;
+    }
+    else
+    {
+      return 1;
+    }
+  }
 
-  // We use "return -1" below to enable compilation.  Change this as you
-  // see fit.
   return -1;
 }
 
-// =====================================================
-// OPTIONAL: Implement "remove" only for bonus marks
-// =====================================================
-
-// Returns true on successful deletion of the entry in BST with
-// matching journey code. Returns false if it cannot find any such element.
-//
-// You may assume that no two elements in the BST have the same journey
-// code, and journey code is the key for insertion and finding in the BST.
-
 bool BST::remove(int jrnyCode)
 {
-  // Implement deletion in BST
-  
-  // You MUST use only BSTLessThan to compare two journey codes.  You
-  // can create a new node containing the journey code to be searched
-  // (along with some arbitrary price) and invoke BSTLessThan on the
-  // current node in the tree that you are inspecting and the new node
-  // to do your comparison.
 
-  // REMEMBER to update the height of appropriate nodes in the tree
-  // after a successful deletion.
-  
-  // We use "return true" below to enable compilation.  Change this as you
-  // see fit.
+  // IF NODE NOT FOUND
+  TreeNode *searchNode = new TreeNode(jrnyCode, -1);
+  TreeNode *currentNode = root;
+  while (currentNode != nullptr)
+  {
+    if (BSTLessThan(searchNode, currentNode))
+    {
+      currentNode = currentNode->left;
+    }
+    else if (BSTLessThan(currentNode, searchNode))
+    {
+      currentNode = currentNode->right;
+    }
+    else
+    {
+      break;
+    }
+  }
 
+  if(currentNode == nullptr){
+    return false;
+  }
+
+  // NO CHILDREN
+  if (!currentNode->left && !currentNode->right)
+  {
+    if (!currentNode->parent)
+    {
+      root = nullptr;
+    }
+    else if (BSTLessThan(currentNode, currentNode->parent))
+    {
+      currentNode->parent->left = nullptr;
+    }
+    else
+    {
+      currentNode->parent->right = nullptr;
+    }
+  }
+
+  // THERE IS NO LEFT CHILD (ONLY RIGHT CHILD IS PRESENT)
+  else if (!currentNode->left)
+  {
+    if (!currentNode->parent)
+    {
+      updateBSTParentChild(currentNode->right, nullptr, true);
+      root = currentNode->right;
+    }
+    else
+    {
+      if (BSTLessThan(currentNode, currentNode->parent))
+      {
+        updateBSTParentChild(currentNode->right, currentNode->parent, true);
+      }
+      else
+      {
+        updateBSTParentChild(currentNode->right, currentNode->parent, false);
+      }
+    }
+  }
+
+  // THERE IS NO RIGHT CHILD (ONLY LEFT CHILD IS PRESENT)
+  else if (!currentNode->right)
+  {
+    if (!currentNode->parent)
+    {
+      updateBSTParentChild(currentNode->left, nullptr, true);
+      root = currentNode->left;
+    }
+    else
+    {
+      if (BSTLessThan(currentNode,  currentNode->parent))
+      {
+        updateBSTParentChild(currentNode->left, currentNode->parent, true);
+      }
+      else
+      {
+        updateBSTParentChild(currentNode->left, currentNode->parent, false);
+      }
+    }
+  }
+
+  // THE NODE HAS BOTH ITS CHILDREN
+  else
+  {
+    TreeNode *successor = currentNode->right;
+    while (successor->left)
+    {
+      successor = successor->left;
+    }
+
+    // DELETE THE SUCCESSOR
+    if (! successor->right)
+    {
+      if (BSTLessThan(successor , successor->parent))
+      {
+        successor->parent->left = nullptr;
+      }
+      else
+      {
+        successor->parent->right = nullptr;
+      }
+    }
+    else
+    {
+
+      if (BSTLessThan(successor, successor->parent))
+      {
+        updateBSTParentChild(successor->right, successor->parent, true);
+      }
+      else
+      {
+        updateBSTParentChild(successor->right, successor->parent, false);
+      }
+    }
+    // COPY SUCCESSOR TO THE CURRENT NODE
+
+    if(currentNode->parent){
+      bool isCurrentNodeLeftChild = BSTLessThan(currentNode, currentNode->parent);
+      updateBSTParentChild(currentNode->left, successor, true);
+      updateBSTParentChild(currentNode->right, successor, false);
+      updateBSTParentChild(successor, currentNode->parent, isCurrentNodeLeftChild);
+    }
+    else{
+      updateBSTParentChild(currentNode->left, successor, true);
+      updateBSTParentChild(currentNode->right, successor, false);
+      updateBSTParentChild(successor, nullptr, true);
+      root = successor;
+    }
+  }
+  
   return true;
 }
-
-
